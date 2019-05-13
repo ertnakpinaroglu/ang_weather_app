d3a320f4a6fb196e81affd071c051a19e599fe0d
ertnakpinaroglu <ertanakpinaroglu@gmail.com>
2019-05-13 18:09:46 +0300

    Weather.services.ts is created by author.

--- Frontend/weather-frontend/src/app/app.module.ts
+++ Frontend/weather-frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from 'src/components/Navbar/Navbar.component';
 import { BodyComponent } from 'src/components/Body/Body.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { BodyComponent } from 'src/components/Body/Body.component';
     BodyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Frontend/weather-frontend/src/services/Weather.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
+import { WeatherModel } from '../models/Weather.model';
+@Injectable({
+  providedIn: 'root'
+})
+
+export class WeatherService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getWeatherByCityName(): Observable<WeatherModel> {
+    return this.httpClient.get<WeatherModel>("http://localhost:8000/getWeather");
+  }
+
+
+
+}
